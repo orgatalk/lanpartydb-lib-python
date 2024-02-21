1245f508693b7bf265821246e225e82d04637031
Jochen Kupperschmidt <homework@nwsnet.de>
2024-02-21 09:13:22 +0100

    Add models

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,5 @@
 
 
 ## 0.1.0 (unreleased)
+
+- Added models.
---
+++ src/lanpartydb/models.py
@@ -0,0 +1,57 @@
+"""
+lanpartydb.models
+~~~~~~~~~~~~~~~~~
+
+Data models
+
+:Copyright: 2024 Jochen Kupperschmidt
+:License: MIT
+"""
+
+from __future__ import annotations
+from dataclasses import dataclass, field
+from datetime import date
+from decimal import Decimal
+
+
+@dataclass(frozen=True)
+class Series:
+    slug: str
+    name: str
+    alternative_names: set[str] = field(default_factory=set)
+
+
+@dataclass(frozen=True, kw_only=True)
+class Party:
+    slug: str
+    title: str
+    series_slug: str | None = field(kw_only=True, default=None)
+    organizer_entity: str | None = field(kw_only=True, default=None)
+    start_on: date
+    end_on: date
+    seats: int | None = None
+    online: bool | None = False
+    location: Location | None = None
+    links: Links | None = None
+
+
+@dataclass(frozen=True)
+class Location:
+    name: str | None = field(kw_only=True, default=None)
+    country_code: str
+    city: str
+    zip_code: str | None = None
+    street: str | None = None
+    latitude: Decimal | None = None
+    longitude: Decimal | None = None
+
+
+@dataclass(frozen=True)
+class Links:
+    website: Website | None
+
+
+@dataclass(frozen=True)
+class Website:
+    url: str
+    offline: bool = False
